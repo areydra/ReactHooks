16294501812bb4805656c27c25d51f60ed009e0a
Areydra Desfikriandre <areydra@gmail.com>
2022-02-16 21:40:37 +0700

    create useReducer implementation

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import { SafeAreaView } from 'react-native';
 import LazyStateScene from './src/scenes/LazyStateScene';
 import CustomHookScene from './src/scenes/CustomHookScene';
 import ErrorBoundaryScene from './src/scenes/ErrorBoundaryScene';
+import UseReducerScene from './src/scenes/UseReducerScene';
 
 const App = () => (
   <SafeAreaView style={{flex: 1}}>
     {/* <LazyStateScene/> */}
     {/* <CustomHookScene/> */}
     {/* <ErrorBoundaryScene/> */}
+    {/* <UseReducerScene/> */}
   </SafeAreaView>
 );
 
